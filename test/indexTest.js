676171b73330f207c3d32aa45e71f8281794a926
Chaithu-123 <yakasirick@gmail.com>
2018-07-31 13:57:39 +0530

    Test

--- test/indexTest.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const index = require('../index');
-
-describe('Index', function(){
-    it('it should return hello1', function(){
-        assert.equal(index(), 'hello1');
-    })
-})
