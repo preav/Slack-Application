23090149c8a9db0513ce9ff6a37d6e7eb2cba697
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-08-02 17:18:33 +0530

    Adding dashboard with static content

--- client/src/components/onboarding/dashboard/dashboard-view.js
+++ client/src/components/onboarding/dashboard/dashboard-view.js
@@ -4,9 +4,35 @@ export const dashboardViewHolderId = 'playGround';
 
 export function dashboardComponent() {
   const dashboardElement = createHTMLElement(
-    `<div>
-        <div>
-            <span>Welcome to dashboard</span>
+    `<div class="dashboard d-flex flex-fill justify-content-center">
+        <div class="col-5">    
+            <div class="mt-5">
+                <div class="mt-2">
+                    <img src="./client/src/img/create_a_team.png" alt="Create a team">
+                    <h1>Create a new workspace</h1>
+                    <p>To make a workspace from scratch, please confirm your email address.</p>
+                    <div id="teams" class="d-flex justify-content-center flex-row">
+                        <input type="text" class="form-control mr-3" placeholder="your-team-name" /> 
+                        <button class="btn btn-outline-success">Create Team</button>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-5">
+            <div>
+                <h1>Your workspaces</h1>
+                <p>You're already a member of these Slack workspaces:</p>
+                <div id="teams" class="d-flex justify-content-center flex-column">
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                    <a class="team-link" href="javascript:void(0)">Your Team One</a>
+                </div>
+            </div>
+            </div>
         </div>  
     </div>`,
   );
