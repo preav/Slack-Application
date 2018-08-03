9aa957288a5a116f77ebcf5f60add756c02c803e
kudums <kudum.srani@gmail.com>
2018-08-03 16:47:59 +0530

    Notification on date initial commit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -8,4 +8,23 @@ import 'bootstrap';
 
 import 'jquery-ui/ui/disable-selection';
 
+
+$("#end").change(function () {
+    var startDate = document.getElementById("start").value;
+    var endDate = document.getElementById("end").value;
+ 
+    if ((Date.parse(endDate) <= Date.parse(startDate))) {
+        alert("End date should be greater than Start date");
+        document.getElementById("end").value = "";
+    }
+});
+
+
+
+
+
+
+import '../../../firebase/firebase';
+
+
 require('font-awesome/css/font-awesome.css');
