a4ce876f82f2c27a82cfc3a580f56f3b49ff19da
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-08-07 11:21:37 +0530

    Updating functions

--- functions/index.js
+++ functions/index.js
@@ -22,56 +22,56 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 //saveUpdateUser?userId=myuserid6
 // **JSON FORMAT**
 // {
-// 	"data":
-// 	{
-// 		"username":"onetwothreefour",
-// 		"email":"xyz@mail.com",
-// 		"phone":"239472974927"
-// 	},
-// 	"action":"UPDATE_USER"
+//     "username":"onetwothreefour",
+//     "email":"xyz@mail.com",
+//     "phone":"239472974927"
 // }
 exports.saveUpdateUser = functions.https.onRequest((request, response) => {
     cors(request, response, () => {});
     const userId = request.query.userId;
-    const data = request.body.data;
-    const action = request.body.action;
+    const userData = request.body;
     console.log("userId: "+userId);
     console.log(request.query);
     console.log(request.body);
-    console.log("Action: "+action);
 
-    switch(action)
-    {
-        case "CREATE_USER":
-        return admin.database().ref('/users/'+userId).set(data, (error)  => {
-            //console.log(snapshot);
-            if(error)
-            {
-                console.log("Error occurred while saving user: "+error);
-                return response.json("Error occurred while saving user: "+error);
-            }
-            else
-            {
-                console.log("User saved successfully");
-                return response.json("User saved successfully");
-            }
-        });
-        case "UPDATE_USER":
-        return admin.database().ref('/users/'+userId).update(data, (error)  => {
-            //console.log(snapshot);
-            if(error)
-            {
-                console.log("Error occurred while updating user: "+error);
-                return response.json("Error occurred while updating user: "+error);
-            }
-            else
-            {
-                console.log("User updated successfully");
-                return response.json("User updated successfully");
-            }
-        });
-    }
-    
+    admin.database().ref('/users/'+userId).once('value', (snapshot) => {
+        var data = snapshot.val();
+        console.log(data);
+        console.log(snapshot.val() === null);
+
+        if(snapshot.val() === null)
+        {
+            return admin.database().ref('/users/'+userId).set(userData, (error)  => {
+                //console.log(snapshot);
+                if(error)
+                {
+                    console.log("Error occurred while saving user: "+error);
+                    return response.json(error);
+                }
+                else
+                {
+                    console.log("User saved successfully");
+                    return response.json("User saved successfully");
+                }
+            });
+        }
+        else
+        {
+            return admin.database().ref('/users/'+userId).update(userData, (error)  => {
+                //console.log(snapshot);
+                if(error)
+                {
+                    console.log("Error occurred while updating user: "+error);
+                    return response.json(error);
+                }
+                else
+                {
+                    console.log("User updated successfully");
+                    return response.json("User updated successfully");
+                }
+            });
+        }
+     });
 });
 
 // Get user Info
