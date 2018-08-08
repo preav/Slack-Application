302a868baea2bbac950a84a11fba31b701ca889e
kudums <kudum.srani@gmail.com>
2018-08-08 17:37:05 +0530

    ref nulll resolved

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -23,16 +23,6 @@ import './slackbot/main';
 
 import './chats/chat-service';
 
-$("#end").change(function () {
-    var startDate = document.getElementById("start").value;
-    var endDate = document.getElementById("end").value;
- 
-    if ((Date.parse(endDate) <= Date.parse(startDate))) {
-        alert("End date should be greater than Start date");
-        document.getElementById("end").value = "";
-    }
-});
-
 import 'firebase';
 
 import '../../../firebase/firebase';
