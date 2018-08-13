cc49b750a4b0e0c0b58613262b9e88b026309d7b
Chaithu-123 <yakasirick@gmail.com>
2018-08-13 15:44:33 +0530

    notify working

--- client/src/components/chats/chat-service.js
+++ client/src/components/chats/chat-service.js
@@ -42,7 +42,7 @@ export function openChatDetailsForUser(userId, teamID) {
             if ((childData.sentByUserName === sentToUserName || childData.sentToUserName === sentToUserName) &&
                 (childData.sentByUserName === userName || childData.sentToUserName === userName)) {
                 renderMessage(childSnapshot, chatBox);
-                console.log(childData)
+                // console.log(childData)
                 if (childSnapshot.val().sentByUserName !== userName) {
                     var msgInfo = {
                         "messageText": childSnapshot.val().messageText,
